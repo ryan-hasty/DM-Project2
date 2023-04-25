f3391fe0854b388366920e11083b26afb75e46a5
Nick Paternoster <70091428+PatJam21@users.noreply.github.com>
2023-04-25 01:53:27 -0500

    slow H cluster

--- hierarchical.py
+++ hierarchical.py
@@ -1,39 +1,63 @@
-import data as d
-import pandas as pd
+import data as data
 import numpy as np
+from scipy.spatial.distance import pdist, squareform
+
+def HierarchicalHub(dataset):
+    #Add the first set of clusters, every data point is a cluster
+    Clustertree = data.ClusterTree()
+    Clustertree.clusters = np.array([dataset])
+    #Find the min of the first two closest clusters, and combine them
+    min_row_idx, min_col_idx = CalculateDistance(dataset)
+    newclusters = MergeClosestCluster(min_row_idx, min_col_idx, dataset)
+    Clustertree.clusters = np.append(Clustertree.clusters, newclusters)
+
+    while(len(newclusters.cluster) > 1):
+        min_row_idx, min_col_idx = CalculateDistance(newclusters)
+        newclusters = MergeClosestCluster(min_row_idx, min_col_idx, newclusters)
+        Clustertree.clusters = np.append(Clustertree.clusters, newclusters)
+
+    return Clustertree
 
 
-def hierarchical_clustering(dataset):
+def CalculateDistance(clusters):
+    centroids = np.array([c.centroid for c in clusters.cluster]) # get the centroids of all clusters
+    distance = pdist(centroids) # compute pairwise distances between centroids
+    
+    # Convert the condensed distance matrix to a square distance matrix
+    distance = squareform(distance, force='nochecks')
+    
+    # Set the diagonal elements to infinity to exclude comparisons with itself
+    np.fill_diagonal(distance, np.inf) 
+    
+    min_row_idx, min_col_idx = FindMinDistance(distance)
+    print(f"The two clusters with the smallest distance are {min_row_idx} and {min_col_idx}")
+    
+    clusters.amount = len(clusters.cluster)
+    clusters.averagedistance = AverageDistance(distance)
+    
+    return min_row_idx, min_col_idx
 
-    # use clustered_data to modify values instead of actual dataset
-    clustered_data = np.copy(dataset)
-    iteration_count = 0  # for later use?
 
-    """Not sure how to get the amount of clusters that we have per iteration... length of dataset will not work"""
+def FindMinDistance(distance):
+    # Get the flattened upper triangle of the distance matrix
+    flattened_dist = distance[np.triu_indices(distance.shape[0], k=1)] 
+    # Get the index of the minimum distance in the flattened array
+    min_dist_idx = np.argmin(flattened_dist) 
+    # Convert the flattened index to the row and column indices in the distance matrix
+    min_row_idx, min_col_idx = np.unravel_index(min_dist_idx, (distance.shape[0], distance.shape[0])) 
+    return min_row_idx, min_col_idx
 
-    # While the amount of clusters > 1, we want to cluster them together and count iterations
-    while (len(clustered_data.data > 1)):
-        smallest = 9999  # Random large value prior to going through set
-        for x in clustered_data.data:  # for every item in dataset
-            for j in clustered_data.data:  # check it against every other item
-                # if distance = 0 then they are same point? fix
-                if np.linalg.norm(x-j) == 0:
-                    continue
-                else:
-                    # if it's smaller then the current smallest distance
-                    if np.linalg.norm(x-j) < smallest:
-                        # then it's the new smallest distance
-                        smallest = np.linalg.norm(x-j)
-                        # get indexes of smallest variables
-                        small1 = np.where(dataset == x)
-                        # get indexes of smallest variables
-                        small2 = np.where(dataset == j)
+def AverageDistance(distance):
+    return np.mean(distance)
 
-        # add the new "centroid" / Average between two points
-        clustered_data = np.append(clustered_data, np.mean(dataset.data[small1, small2]))
-        # remove smallest point 1 - Since centroid is replacing them
-        clustered_data = np.delete(clustered_data, dataset.data[small1])
-        # remove smallest point 2 - Since centroid is replacing them
-        clustered_data = np.delete(clustered_data, dataset.data[small2])
-        # add one to iteration_count
-        iteration_count += 1
+def MergeClosestCluster(min_row_idx, min_col_idx, clusters):
+    newcluster = data.Cluster()
+    newcentroid = np.mean([clusters.cluster[min_row_idx].centroid, clusters.cluster[min_col_idx].centroid], axis=0)
+    newcluster.centroid = newcentroid
+    
+    # Create new clusters object with all clusters except the two being merged
+    newclusters = data.Clusters()
+    newclusters.cluster = np.delete(clusters.cluster, [min_row_idx, min_col_idx])
+    newclusters.cluster = np.concatenate((newclusters.cluster, [newcluster]))
+
+    return newclusters
